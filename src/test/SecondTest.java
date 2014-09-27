24263e00333bd19c9ac271cfdf89f3f73cd7bd2f
YeshiChetanR <yeshichetanr@gmail.com>
2014-09-27 18:59:11 +0530

    adding files

---
+++ src/test/SecondTest.java
@@ -0,0 +1,10 @@
+package test;
+import org.testng.annotations.Test;
+
+
+public class SecondTest {
+@Test
+	public void secondTest(){
+	System.out.println("SecondTest");
+}
+}
---
+++ src/test/TestingHere.java
@@ -0,0 +1,12 @@
+package test;
+import org.testng.annotations.Test;
+
+
+public class TestingHere {
+	
+	@Test
+	public void main( ) {
+		System.out.println("First Test");
+	}
+
+}
